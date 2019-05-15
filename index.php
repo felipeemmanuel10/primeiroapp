bd6e9a458a600ec178085fea323cae0e2fbeb5e8
Roney Ferreira <leonardofreitas159@gmail.com>
2019-05-14 21:41:16 -0300

    Pronto para o Heroko

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+    <h1>Thiago cu de Tábua</h1>
+
+</head>
+<body>
+    <?php echo "Essa parte está sendo em PHP";
+    ?>
+</body>
+</html>
